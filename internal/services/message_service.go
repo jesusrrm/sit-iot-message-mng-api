c335d81eb5b834f97359004114edd25ad4d56205
Jesus Redon <jesusrrm@gmail.com>
2025-08-12 09:53:16 +0200

    removed unused function

--- internal/services/message_service.go
+++ internal/services/message_service.go
@@ -9,8 +9,6 @@ import (
 
 type MessageService interface {
 	GetMessageByID(ctx context.Context, id string) (*models.Message, error)
-	ListMessages(ctx context.Context, filter map[string]interface{}, sortField, sortOrder string, skip, limit int) ([]*models.Message, int, error)
-	ListMessagesByProjectID(ctx context.Context, projectID string, filter map[string]interface{}, sortField, sortOrder string, skip, limit int) ([]*models.Message, int, error)
 	ListMessagesByDeviceID(ctx context.Context, deviceID string, filter map[string]interface{}, sortField, sortOrder string, skip, limit int) ([]*models.Message, int, error)
 	VerifyToken(ctx context.Context, token string) (*auth.Token, error)
 }
